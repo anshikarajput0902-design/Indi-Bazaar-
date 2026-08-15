53e31b5de68c0e032bb4379ccbb22bafd73229dc
Anshika Rajput <anshikarajput0902@gmail.com>
2026-08-15 17:25:02 +0530

    update base path

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/Indi-Bazaar-/',
+    base: './',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
