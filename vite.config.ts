a6e5890c1d486ce5c284afe900749b30e1629f37
Anshika Rajput <anshikarajput0902@gmail.com>
2026-08-15 16:49:45 +0530

    Fix vite base path

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/Indi-Bazaar-/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
